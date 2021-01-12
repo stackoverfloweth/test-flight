a617c7b48f2eb97a6b56618d016591cf86b75ed4
Shaun Davis <davissp14@Shauns-MacBook-Pro.local>
2021-01-12 11:00:46 -0600

    Cleaning up newlines

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,20 +5,16 @@ class Contact < ApplicationRecord
   validates_presence_of :firstName, :lastName, :phone, :email
   validates_length_of :phone, is: 10
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-
   # Ensures an account can only have as single primary contact.
   validates_uniqueness_of :primary, scope: :account_id, conditions: -> { where(primary: true) } 
-  
   # Global Uniqueness Constraint
   # This should be handled via a unique index on the db model, but we can re-enforce that here.
   # First step would be to identify what field(s) we could use to enforce global uniqueness.
   # Seems like we could use the Phone number, email address or both.    
   validates_uniqueness_of :phone
-  validates_uniqueness_of :email
-
+  validates_uniqueness_of :emailex
 
   before_validation :sanitize_phone_number
-  
   
   def is_primary?
     # Truthy allows us to treat nil and false the same, but passing nil to ruby methods that 
@@ -29,7 +25,6 @@ class Contact < ApplicationRecord
   end
 
   private 
-
 
   def sanitize_phone_number
     self.phone = phone.gsub(/\W/, '')
